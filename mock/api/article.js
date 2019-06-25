3ec8fbe84f73af7d20e7aa278ec7827909a222e7
duanyuting <duanyuting@utry.cn>
2019-06-25 16:53:03 +0800

    mock数据

--- mock/api/article.js
+++ mock/api/article.js
@@ -1,33 +1,37 @@
 /* eslint-disable */
-
 // http://mockjs.com/examples.html
-import Mock from "mockjs";
-
-//
-//
-export const delay = 0;
-export const disabled = false;
+import Mock from 'mockjs' //
 //
 
-
+export const delay = 0
+export const disabled = false //
 //
 export default {
   //
-  "GET /api/article/getList": (req, res, next) => {
-    const { id } = params
+  'GET /api/article/getList': (req, res, next) => {
+    const { query } = req
+    const { currentPage, pageSize } = query
     return Mock.mock({
-      "Data|20-100": [
+      'Data|40-200': [
         {
           date: '@date("yyyy-MM-dd")',
-          name: "@cname(2, 4)",
-          address: "@county(true)",
-          currentPage: params.currentPage,
-          flag: "@boolean"
-        }
+          name: '@cname(2, 4)',
+          address: '@county(true)',
+          flag: '@boolean',
+        },
       ],
+
       count() {
-        return this.Data.length;
-      }
-    });
-  }
-};
+        return this.Data.length
+      },
+
+      currentPage() {
+        return +currentPage
+      },
+
+      pageSize() {
+        return +pageSize
+      },
+    })
+  },
+}
