a65281cb30f0549734aa2e8484f5d4f6a7ca697d
Della156 <della_i@163.com>
2019-06-28 11:43:06 +0800

    fit: mock数据table问题

--- mock/api/article.js
+++ mock/api/article.js
@@ -6,32 +6,37 @@ import Mock from 'mockjs' //
 export const delay = 0
 export const disabled = false //
 //
+
+const articleData = Mock.mock({
+  'Data|40-200': [
+    {
+      date: '@date("yyyy-MM-dd")',
+      name: '@cname(2, 4)',
+      address: '@county(true)',
+      flag: '@boolean',
+    },
+  ],
+
+  count() {
+    return this.Data.length
+  },
+})
+
 export default {
   //
   'GET /api/article/getList': (req, res, next) => {
     const { query } = req
     const { currentPage, pageSize } = query
-    return Mock.mock({
-      'Data|40-200': [
-        {
-          date: '@date("yyyy-MM-dd")',
-          name: '@cname(2, 4)',
-          address: '@county(true)',
-          flag: '@boolean',
-        },
-      ],
-
-      count() {
-        return this.Data.length
-      },
-
-      currentPage() {
-        return +currentPage
-      },
-
-      pageSize() {
-        return +pageSize
-      },
-    })
+    let artData = articleData.Data
+    const starIndex = (currentPage - 1) * pageSize
+    const lastIndex = starIndex + Number(pageSize)
+    let Data = artData.slice(starIndex, lastIndex)
+    let count = articleData.count
+    return {
+      Data,
+      count,
+      currentPage: +currentPage,
+      pageSize: +pageSize,
+    }
   },
 }
